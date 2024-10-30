a4e82900a966eadbf91129197667e620cbed35a5
Manuel Ventura <manuueelneto@gmail.com>
2024-10-30 13:18:55 -0300

    cards pets

--- ong_animais/urls.py
+++ ong_animais/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from ong_animais.views import home, sobre, contato
+from ong_animais.views import home, sobre, contato, cadastra_pet, deleta_pet
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', home, name='home'),
     path('sobre', sobre, name='sobre'),
     path('contato', contato, name='contato'),
+    path('cadastra_pet', cadastra_pet, name='cadastra_pet'),
+    path('deleta_pet', deleta_pet, name='deleta_pet'),
 ]
 
 if settings.DEBUG:
--- ong_animais/views.py
+++ ong_animais/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Pet
+from .forms import PetForms
+from django.contrib import messages
+
 def home(request):
     pets = Pet.objects.all()
     return render(request, 'home/home.html', {'pets': pets})
@@ -9,3 +12,17 @@ def sobre(request):
 
 def contato(request):
     return render(request, 'informacoes/contato.html')
+
+def cadastra_pet(request):
+    if not request.user.is_authenticated:
+        return redirect('home')
+    form = PetForms
+    
+    if request.method == 'POST':
+        form = PetForms(request.POST, request.FILES)
+        if form.is_valid:
+            form.save()
+    return render(request, 'pets/cadastra_pet.html', {'form': form})
+
+def deleta_pet(request):
+    return render(request, 'pets/deleta_pet.html')
