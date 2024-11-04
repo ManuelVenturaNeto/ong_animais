2c2987510b64e36e435a55fcc14754c5add0ceb6
Manuel Ventura <manuueelneto@gmail.com>
2024-11-04 15:16:40 -0300

    problemas na posicao da propaganda e problemas nos botoes de cadastro

--- setup/settings.py
+++ setup/settings.py
@@ -126,7 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # This should contain your development static files.
+]
 
 # Configurações de mídia
 MEDIA_URL = '/media/'
